46813468a98216dc3b4794cea0a037f4aef928a5
jvitorfranca <vitoorbr@gmail.com>
2020-06-30 18:07:32 -0300

    Adicionando progresso do capítulo 3

---
+++ app/models/tables.py
@@ -0,0 +1,22 @@
+from app import db
+
+
+class Pessoa(db.model):
+    __tablename__ = 'pessoas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    idade = db.Column(db.Integer)
+    sexo = db.Column(db.String(1))
+    salario = db.Column(db.Float)
+
+    def __init__(self, nome='Anônimo', idade=18, sexo='M', salario=1050):
+
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+        self.salario = salario
+
+    def __repr__(self):
+
+        return '<Pessoa %r>' % self.nome
